032251b55eae0dcdadd73717c1b776eeb6bb539e
AndreiKuzmenko <eccehomo28@gmail.com>
2023-08-14 15:51:48 +0300

    14082023-POM

--- src/main/java/screens/ContactListScreen.java
+++ src/main/java/screens/ContactListScreen.java
@@ -5,6 +5,7 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.touch.offset.PointOption;
 import models.Contact;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Rectangle;
 import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
@@ -70,6 +71,7 @@ public class ContactListScreen extends BaseScreen{
         }
         return false;
     }
+
 
     public ContactListScreen removeOneContact(){
         waitElement(addContactBtn, 5);
@@ -159,6 +161,25 @@ public class ContactListScreen extends BaseScreen{
                 .viewContactObject();
         driver.navigate().back();
         return contact.toString().contains(text);
+    }
+
+
+    public ContactListScreen ScrollingToLastContact(){
+        Dimension dimensions = driver.manage().window().getSize();
+        waitElement(addContactBtn, 5);
+        MobileElement contact = contacts.get(7);
+        Rectangle rect = contact.getRect();
+
+        int start_y = (int) (dimensions.height * 0.2);
+        int end_y = (int) (dimensions.height * 0.95);
+
+        TouchAction<?> touchAction = new TouchAction<>(driver);
+        touchAction.press(0, start_y)
+                .moveTo(0, end_y)
+                .release()
+                .perform();
+
+
     }
 
 }
